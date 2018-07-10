821b4e4d3084ed0cdac6e6eaa45de9ab8ff800f0
Robin <bin.googol@gmail.com>
2018-07-10 22:51:05 +0800

    queues init

---
+++ queues/Deque.java
@@ -0,0 +1,36 @@
+import java.util.Iterator;
+
+public class Deque<Item> implements Iterable<Item> {
+    public Deque() {
+
+    }
+
+    public boolean isEmpty() {
+
+    }
+
+    public void addFirst(Item item) {
+
+    }
+
+    public void addLast(Item item) {
+
+    }
+
+    public Item removeFirst() {
+
+    }
+
+    public Item removeLast() {
+
+    }
+
+    @Override
+    public Iterator<Item> iterator() {
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}

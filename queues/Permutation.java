09c74bbc958e2b47592c014cf839cce86dc03a01
Robin <bin.googol@gmail.com>
2018-07-11 20:54:15 +0800

    Permutation

--- queues/Permutation.java
+++ queues/Permutation.java
@@ -1,5 +1,22 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+import java.util.NoSuchElementException;
+
 public class Permutation {
     public static void main(String[] args) {
-
+        int k = Integer.parseInt(args[0]);
+        RandomizedQueue<String> rq = new RandomizedQueue();
+        while (true) {
+            try {
+                rq.enqueue(StdIn.readString());
+            }
+            catch (NoSuchElementException e) {
+                break;
+            }
+        }
+        for (int i = 0; i < k; i++) {
+            StdOut.println(rq.dequeue());
+        }
     }
 }

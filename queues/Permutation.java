f8427d2e995a56f6985e34953e9144c4f4b23bba
Robin <bin.googol@gmail.com>
2018-07-11 21:27:59 +0800

    Check empty before read

--- queues/Permutation.java
+++ queues/Permutation.java
@@ -1,19 +1,12 @@
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
 
-import java.util.NoSuchElementException;
-
 public class Permutation {
     public static void main(String[] args) {
         int k = Integer.parseInt(args[0]);
         RandomizedQueue<String> rq = new RandomizedQueue();
-        while (true) {
-            try {
-                rq.enqueue(StdIn.readString());
-            }
-            catch (NoSuchElementException e) {
-                break;
-            }
+        while (!StdIn.isEmpty()) {
+            rq.enqueue(StdIn.readString());
         }
         for (int i = 0; i < k; i++) {
             StdOut.println(rq.dequeue());

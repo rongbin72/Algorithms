fb97f43806e44258786a62d438d8455a5492ffb8
Robin <bin.googol@gmail.com>
2018-07-11 10:47:24 +0800

    RandomizedQueue init

--- queues/RandomizedQueue.java
+++ queues/RandomizedQueue.java
@@ -6,11 +6,11 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
     }
 
     public boolean isEmpty() {
-
+        return false;
     }
 
     public int size() {
-
+        return 0;
     }
 
     public void enqueue(Item item) {
@@ -18,15 +18,15 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
     }
 
     public Item dequeue() {
-
+        return null;
     }
 
     public Item sample() {
-
+        return null;
     }
 
     public Iterator<Item> iterator() {
-
+        return null;
     }
 
     public static void main(String[] args) {

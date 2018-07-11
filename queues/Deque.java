6659265242fb350b3f9ed26f3eefb2276de16b96
Robin <bin.googol@gmail.com>
2018-07-11 10:41:05 +0800

    Deque init

--- queues/Deque.java
+++ queues/Deque.java
@@ -1,33 +1,124 @@
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public class Deque<Item> implements Iterable<Item> {
-    public Deque() {
+    private int n;
+    private Node first;
+    private Node last;
 
+    class Node {
+        private Item item;
+        private Node prev;
+        private Node next;
+    }
+
+    public Deque() {
+        first = null;
+        n = 0;
+        assert check();
     }
 
     public boolean isEmpty() {
-
+        return first == null;
     }
 
     public void addFirst(Item item) {
+        if (item == null) throw new IllegalArgumentException();
 
+        Node oldFirst = first;
+        first = new Node();
+        first.item = item;
+        first.next = oldFirst;
+        oldFirst.prev = first;
+        n++;
+        assert check();
     }
 
     public void addLast(Item item) {
+        if (item == null) throw new IllegalArgumentException();
 
+        Node oldLast = last;
+        last = new Node();
+        last.item = item;
+        last.prev = oldLast;
+        oldLast.next = last;
+        n++;
+        assert check();
     }
 
     public Item removeFirst() {
+        if (isEmpty()) throw new NoSuchElementException();
 
+        Item item = first.item;
+        first = first.next;
+        first.prev = null;
+        n--;
+        assert check();
+        return item;
     }
 
     public Item removeLast() {
+        if (isEmpty()) throw new NoSuchElementException();
 
+        Item item = last.item;
+        last = last.prev;
+        last.next = null;
+        n--;
+        assert check();
+        return item;
+    }
+
+
+    private boolean check() {
+        // check a few properties of instance variable 'first'
+        if (n < 0) return false;
+        if (n == 0 && first != null) return false;
+
+        else if (n == 1) {
+            if (first == null || first.next != null)
+                return false;
+        }
+        else {
+            if (first == null || first.next == null)
+                return false;
+        }
+
+        // check internal consistency of instance variable n
+        int numberOfNodes = 0;
+        for (Node x = first; x != null && numberOfNodes <= n; x = x.next) {
+            numberOfNodes++;
+        }
+        if (numberOfNodes != n) return false;
+
+        return true;
     }
 
     @Override
     public Iterator<Item> iterator() {
-        return null;
+        return new DequeIterator();
+    }
+
+    public class DequeIterator implements Iterator<Item> {
+        private Node current = first;
+
+        @Override
+        public boolean hasNext() {
+            return current != null;
+        }
+
+        @Override
+        public Item next() {
+            if (!hasNext()) throw new NoSuchElementException();
+
+            Item item = current.item;
+            current = current.next;
+            return item;
+        }
+
+        @Override
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
     }
 
     public static void main(String[] args) {

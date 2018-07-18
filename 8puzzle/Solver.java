eae514ee90b2af5036f54780cb6ffd30b73bfaae
Robin <bin.googol@gmail.com>
2018-07-18 12:26:50 +0800

    Solver init

--- 8puzzle/Solver.java
+++ 8puzzle/Solver.java
@@ -6,13 +6,13 @@ public class Solver {
 
     }
     public boolean isSolvable() {
-
+        return false;
     }
     public int moves() {
-
+        return 0;
     }
     public Iterable<Board> solution() {
-
+        return null;
     }
     public static void main(String[] args) {
         // create initial board from file
